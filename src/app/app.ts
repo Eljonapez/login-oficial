07b9c2b7ffd38c84bb69ff80a47ef8ab8b8bd9a8
Eljonapez <foxynightmare2016@gmail.com>
2025-06-22 05:25:33 -0400

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
+import { Login } from './auth/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Login, Dashboard],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
+
 export class App {
   protected title = 'project';
-}
+
+  }
